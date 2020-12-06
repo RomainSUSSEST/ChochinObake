f8b553bd396f3f50a9e4ab8ce26c7f4a9b4ebf73
=Maxime <maxmo@orange.fr>
2020-12-06 18:55:05 +0100

    Modif ambiance

---
+++ StS/Assets/ASuppr/InGameCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameCamera : MonoBehaviour
+{
+    float skyboxStartAngle = 30f;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time);
+    }
+}
